37afe5ee62198dbda19d0878528d51fd3d944b28
C-Mierez <carlos.mierez20@gmail.com>
2022-11-05 00:29:36 -0300

    Removed Strict Mode

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,11 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    reactStrictMode: false,
+    swcMinify: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
 };
 export default config;
