9de823af5d94c52a8efdd96f54c529255a01cab3
C-Mierez <carlos.mierez20@gmail.com>
2022-11-06 00:04:31 -0300

    Kinda went crazy with animations. Also landing page

---
+++ src/components/PageRevealer.tsx
@@ -0,0 +1,14 @@
+import css from "../styles/revealer.module.css";
+
+export default function PageRevealer() {
+    return (
+        <>
+            <div className={css.revealer}>
+                <div className={css.revealer_inner}></div>
+            </div>
+            <div className={css.revealer_right}>
+                <div className={css.revealer_inner_right}></div>
+            </div>
+        </>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import css from "../styles/index.module.css";
 import { MutableRefObject } from "react";
 import Card from "../components/Card";
+import PageRevealer from "../components/PageRevealer";
 
 export default function HomePage() {
     const scrollToElement = (ref: MutableRefObject<any>) => {
@@ -17,37 +18,43 @@ export default function HomePage() {
     const sectionLink = useRef(null);
 
     return (
-        <>
-            <div className={css.gradient}></div>
-            <div className={css.landing}>
-                <div className={`${css.slice} ${css.desc}`}>
-                    <p>Keep your finances in one single place</p>
-                </div>
-                <div className={`${css.slice} ${css.title}`}>
-                    <p>
-                        <span className={css.colored}>Analyze</span> your
-                        portfolio performance
-                    </p>
-                    <img
-                        className={css.arrows}
-                        onClick={() => scrollToElement(sectionTrack)}
-                        src="svg/Arrows.svg"
-                        alt="Arrows"
-                    />
+        <div>
+            {/* <div className={css.gradient}></div> */}
+            <PageRevealer />
+            <div className={css.navigation}>
+                <div className={css.advance_button} onClick={() => {}}>
+                    <svg
+                        viewBox="0 0 29 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M28.9912 8.92304L28.9912 0.771729L14.5 9.82874L0.00878859 0.771727L0.00878823 8.92303L14.5 17.98L28.9912 8.92304Z" />
+                    </svg>
                 </div>
             </div>
-            <div className={css.section} ref={sectionTrack}>
+            <div className={css.landing}>
+                <div className={css.background}></div>
+                <h1>All your Finances</h1>
+                <h2>in one single place</h2>
+                <p>
+                    A tool in which to{" "}
+                    <span className={css.colored}>manage</span> your assets and{" "}
+                    <span className={css.colored}>visualize</span> their
+                    performance
+                </p>
+                <div className={css.colored_spacer}></div>
+            </div>
+        </div>
+    );
+}
+
+{
+    /* <div className={css.section} ref={sectionTrack}>
                 <div className={`${css.slice} ${css.title}`}>
                     <p>
                         <span className={css.colored}>Track</span> your open
                         positions
                     </p>
-                    <img
-                        className={css.arrows}
-                        onClick={() => scrollToElement(sectionOrganize)}
-                        src="svg/Arrows.svg"
-                        alt="Arrows"
-                    />
                 </div>
                 <div className={`${css.slice}`}>
                     <Card />
@@ -62,12 +69,6 @@ export default function HomePage() {
                         <span className={css.colored}>Organize</span> into
                         multiple categories
                     </p>
-                    <img
-                        className={css.arrows}
-                        onClick={() => scrollToElement(sectionLink)}
-                        src="svg/Arrows.svg"
-                        alt="Arrows"
-                    />
                 </div>
             </div>
             <div className={css.section} ref={sectionLink}>
@@ -80,7 +81,5 @@ export default function HomePage() {
                 <div className={`${css.slice}`}>
                     <p>Keep your finances in one single place</p>
                 </div>
-            </div>
-        </>
-    );
+            </div> */
 }
