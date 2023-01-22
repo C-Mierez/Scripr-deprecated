93a28c96cd73ceaac6c47fb0e6abf78d93e85e87
C-Mierez <carlos.mierez20@gmail.com>
2023-01-22 14:21:21 -0300

    Replaced old Spacer with a Locomotive one

---
+++ src/components/Spacer.tsx
@@ -0,0 +1,64 @@
+import css from "../styles/spacer.module.css";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
+import { useEffect, useRef, useState } from "react";
+
+export default function Spacer() {
+    const { scroll } = useLocomotiveScroll();
+
+    const [heightFactor, setHeightFactor] = useState<number>(1);
+
+    useEffect(() => {
+        if (scroll) {
+            // Subscribe to the scroll event
+            scroll.on("scroll", (instance: any) => {
+                // Avoid unnecessary calculations if there are no elements in view
+                if (Object.keys(instance.currentElements).length > 0)
+                    // Calculate the height factor
+                    setHeightFactor(
+                        1 + (instance.scroll.y / instance.limit.y) * 13
+                    );
+            });
+        }
+        return () => {
+            if (scroll) {
+                scroll.destroy();
+            }
+        };
+    }, [scroll]);
+
+    return (
+        <div
+            id="container"
+            data-scroll-section
+            className={css.spacer_container}
+        >
+            <div
+                data-scroll
+                data-scroll-speed="4.25"
+                data-scroll-position="top"
+                data-scroll-target="#container"
+                data-scroll-call
+                className={css.spacer1}
+                style={{ height: `calc(3vh * ${heightFactor})` }}
+            ></div>
+            <div
+                data-scroll
+                data-scroll-speed="5.25"
+                data-scroll-position="top"
+                data-scroll-target="#container"
+                className={css.spacer2}
+                style={{ height: `calc(3vh * ${heightFactor})` }}
+            ></div>
+            <div
+                data-scroll
+                data-scroll-speed="6.5"
+                data-scroll-position="top"
+                data-scroll-target="#container"
+                className={css.spacer3}
+                style={{
+                    height: `calc(4vh * ${heightFactor})`,
+                }}
+            ></div>
+        </div>
+    );
+}
