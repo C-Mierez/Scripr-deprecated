e801fe13b2905df92cb6c70aecef511b6a170482
C-Mierez <carlos.mierez20@gmail.com>
2023-01-22 21:54:45 -0300

    Reworked Spacer animation

--- src/components/Spacer.tsx
+++ src/components/Spacer.tsx
@@ -14,6 +14,7 @@ export default function Spacer() {
                 // Avoid unnecessary calculations if there are no elements in view
                 if (Object.keys(instance.currentElements).length > 0)
                     // Calculate the height factor
+
                     setHeightFactor(
                         1 + (instance.scroll.y / instance.limit.y) * 13
                     );
@@ -27,38 +28,56 @@ export default function Spacer() {
     }, [scroll]);
 
     return (
-        <div
-            id="container"
-            data-scroll-section
-            className={css.spacer_container}
-        >
+        <>
             <div
-                data-scroll
-                data-scroll-speed="4.25"
-                data-scroll-position="top"
-                data-scroll-target="#container"
-                data-scroll-call
-                className={css.spacer1}
-                style={{ height: `calc(3vh * ${heightFactor})` }}
-            ></div>
-            <div
-                data-scroll
-                data-scroll-speed="6.25"
-                data-scroll-position="top"
-                data-scroll-target="#container"
-                className={css.spacer2}
-                style={{ height: `calc(3vh * ${heightFactor})` }}
-            ></div>
-            <div
-                data-scroll
-                data-scroll-speed="7"
-                data-scroll-position="top"
-                data-scroll-target="#container"
-                className={css.spacer3}
-                style={{
-                    height: `calc(3.5vh * ${heightFactor})`,
-                }}
-            ></div>
-        </div>
+                id="container"
+                data-scroll-section
+                className={css.spacer_container}
+            >
+                <div
+                    data-scroll
+                    data-scroll-speed="1.25"
+                    data-scroll-position="top"
+                    data-scroll-target="#container"
+                    data-scroll-call
+                    className={css.spacer1}
+                    style={{ height: `calc(3vh * ${heightFactor})` }}
+                ></div>
+                <div
+                    data-scroll
+                    data-scroll-speed="2.25"
+                    data-scroll-position="top"
+                    data-scroll-target="#container"
+                    className={css.spacer2}
+                    style={{ height: `calc(3vh * ${heightFactor})` }}
+                ></div>
+                <div
+                    data-scroll
+                    data-scroll-speed="3.5"
+                    data-scroll-position="top"
+                    data-scroll-target="#container"
+                    className={css.spacer3}
+                    style={{
+                        height: `calc(3.5vh * ${heightFactor})`,
+                    }}
+                ></div>
+            </div>
+            <div data-scroll-section className={css.last_spacer}>
+                <div className={css.spacer4}></div>
+                <div className={css.shape}>
+                    <svg
+                        data-name="Layer 1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1200 120"
+                        preserveAspectRatio="none"
+                    >
+                        <path
+                            d="M1200 0L0 0 598.97 114.72 1200 0z"
+                            className={css.shape_fill}
+                        ></path>
+                    </svg>
+                </div>
+            </div>
+        </>
     );
 }
