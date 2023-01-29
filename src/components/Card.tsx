c3e98bd202e9cd7aee049ac8c53fce96e7af0af2
C-Mierez <carlos.mierez20@gmail.com>
2023-01-29 01:56:44 -0300

    Reworked Briefing section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,54 @@
 import { useState } from "react";
 import css from "../styles/components/card.module.css";
 
+import React from "react";
+
+export const Card2 = React.forwardRef<
+    HTMLDivElement,
+    { title: string; description: string }
+>((props, ref) => {
+    const [subtitle, setSubtitle] = useState("");
+
+    // const sentence =
+    //     "See your balance and your profits as the price changes in real time."
+
+    const subtitleWords = props.description.split(" ");
+
+    const createSubtitle = () => {
+        setSubtitle("");
+    };
+
+    const deleteSubtitle = () => {
+        setSubtitle("");
+    };
+
+    return (
+        <div
+            ref={ref}
+            data-scroll
+            data-scroll-repeat="true"
+            data-scroll-class={css.slideIn}
+            className={css.card}
+            onMouseEnter={createSubtitle}
+            onMouseLeave={deleteSubtitle}
+        >
+            <div className={css.card_content}>
+                <h1>{props.title}</h1>
+                <p>
+                    {subtitleWords.map((word, i) => (
+                        <span
+                            key={i}
+                            style={{ transitionDelay: `${80 * i}ms` }}
+                        >
+                            {word}
+                        </span>
+                    ))}
+                </p>
+            </div>
+        </div>
+    );
+});
+
 export default function Card(props: { title: string; description: string }) {
     const [subtitle, setSubtitle] = useState("");
 
@@ -19,6 +67,9 @@ export default function Card(props: { title: string; description: string }) {
 
     return (
         <div
+            data-scroll
+            data-scroll-repeat="true"
+            data-scroll-class={css.slideIn}
             className={css.card}
             onMouseEnter={createSubtitle}
             onMouseLeave={deleteSubtitle}
