9caf8d3b74877e72ade0bcd446a42bf2880028ee
C-Mierez <carlos.mierez20@gmail.com>
2023-01-29 15:45:42 -0300

    ✨ Mobile Navigation Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { useLocomotiveScroll } from "react-locomotive-scroll";
 import css from "../styles/layout.module.css";
 import { RefObject, useEffect, useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import { Squash as Hamburger } from "hamburger-react";
 
 export default function Header(props: {
     sectionRefs: RefObject<HTMLDivElement>[];
@@ -11,20 +13,20 @@ export default function Header(props: {
     const [isScrolling, setIsScrolling] = useState<boolean>(false);
     let currentDelta = 0;
     let activeRefIndex = 0;
-    let scrolling = false;
+    // let scrolling = false;
 
     useEffect(() => {
         if (scroll) {
             scroll.on("scroll", (instance: any) => {
                 // // console.log("Start of call ", instance);
 
-                if (Math.abs(instance.delta.y - instance.scroll.y) < 1) {
-                    scrolling = false;
-                    setIsScrolling(false);
-                } else if (!scrolling) {
-                    scrolling = true;
-                    setIsScrolling(true);
-                }
+                // if (Math.abs(instance.delta.y - instance.scroll.y) < 1) {
+                //     scrolling = false;
+                //     setIsScrolling(false);
+                // } else if (!scrolling) {
+                //     scrolling = true;
+                //     setIsScrolling(true);
+                // }
 
                 // Only run calculation when Delta changes. Scroll value (smoothing) doesn't matter
                 if (currentDelta === instance.delta.y) return;
@@ -77,40 +79,111 @@ export default function Header(props: {
         };
     }, [scroll]);
 
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
-        <header
-            className={`${css.header} ${css.shadow} ${
-                isScrolling ? css.hide : css.show
-            }`}
-        >
-            <div className={css.section}>
-                <div
-                    onClick={() => {
-                        scroll.scrollTo(0);
-                    }}
-                >
-                    Scripr
+        <>
+            <header
+                className={`${css.header} ${css.shadow} ${
+                    isScrolling ? css.hide : css.show
+                }`}
+            >
+                <div className={css.section}>
+                    <div
+                        onClick={() => {
+                            scroll.scrollTo(0);
+                            setShowMenu(false);
+                        }}
+                    >
+                        Scripr
+                    </div>
                 </div>
-            </div>
-            <div className={css.section}>
-                <div
-                    className={currentActiveRefIndex == 2 ? css.active : ""}
-                    onClick={() => {
-                        scroll.scrollTo(props.sectionRefs[2]?.current);
-                    }}
-                >
-                    About
+                <div className={css.section}>
+                    <div
+                        className={currentActiveRefIndex == 2 ? css.active : ""}
+                        onClick={() => {
+                            scroll.scrollTo(props.sectionRefs[2]?.current);
+                        }}
+                    >
+                        About
+                    </div>
+                    <div
+                        className={currentActiveRefIndex == 3 ? css.active : ""}
+                        onClick={() => {
+                            scroll.scrollTo(props.sectionRefs[3]?.current);
+                        }}
+                    >
+                        Contact
+                    </div>
+                    <div className={css.launch}>Launch</div>
                 </div>
-                <div
-                    className={currentActiveRefIndex == 3 ? css.active : ""}
-                    onClick={() => {
-                        scroll.scrollTo(props.sectionRefs[3]?.current);
-                    }}
-                >
-                    Contact
+                <div className={css.menuButton}>
+                    <Hamburger
+                        onToggle={() => {
+                            setShowMenu((s) => !s);
+                        }}
+                        toggled={showMenu}
+                    ></Hamburger>
                 </div>
-                <div className={css.launch}>Launch</div>
-            </div>
-        </header>
+            </header>
+            <CSSTransition
+                in={showMenu}
+                timeout={1000}
+                unmountOnExit
+                classNames={{
+                    enter: css.menuEnter,
+                    enterActive: css.menuEnterActive,
+                    exit: css.menuExit,
+                    exitActive: css.menuExitActive,
+                    enterDone: css.menuEnterDone,
+                }}
+            >
+                <div className={css.menu}>
+                    <div className={css.background}>
+                        <div className={css.layer} />
+                        <div className={css.layer} />
+                        <div className={css.layer} />
+                        <div className={css.layer} />
+                        <div className={css.layer} />
+                    </div>
+                    <div className={css.items}>
+                        <div
+                            className={
+                                currentActiveRefIndex == 1 ? css.active : ""
+                            }
+                            onClick={() => {
+                                scroll.scrollTo(props.sectionRefs[1]?.current);
+                                setShowMenu(false);
+                            }}
+                        >
+                            Features
+                        </div>
+                        <div
+                            className={
+                                currentActiveRefIndex == 2 ? css.active : ""
+                            }
+                            onClick={() => {
+                                scroll.scrollTo(props.sectionRefs[2]?.current);
+                                setShowMenu(false);
+                            }}
+                        >
+                            About
+                        </div>
+                        <div
+                            className={
+                                currentActiveRefIndex == 3 ? css.active : ""
+                            }
+                            onClick={() => {
+                                scroll.scrollTo(props.sectionRefs[3]?.current);
+                                setShowMenu(false);
+                            }}
+                        >
+                            Contact
+                        </div>
+                        <div className={css.launch}>Launch App</div>
+                    </div>
+                </div>
+            </CSSTransition>
+        </>
     );
 }
