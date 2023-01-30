a28d14f2ec6a77fd0df4b3be12ffe7904b24104e
C-Mierez <carlos.mierez20@gmail.com>
2023-01-30 19:56:26 -0300

    🎨 Added alternate state to ellipsis component

--- src/components/Ellipsis.tsx
+++ src/components/Ellipsis.tsx
@@ -1,12 +1,18 @@
 import css from "../styles/components/ellipsis.module.css";
 
 // This is ugly but I had to to do it to animate each dot individually :(
-export default function Ellipsis() {
+export default function Ellipsis(props: { alternate: boolean }) {
     return (
         <div className={css.ellipsis}>
-            <div className={css.dot} />
-            <div className={css.dot} />
-            <div className={css.dot} />
+            <div
+                className={`${css.dot} ${props.alternate ? css.alternate : ""}`}
+            />
+            <div
+                className={`${css.dot} ${props.alternate ? css.alternate : ""}`}
+            />
+            <div
+                className={`${css.dot} ${props.alternate ? css.alternate : ""}`}
+            />
         </div>
     );
 }
